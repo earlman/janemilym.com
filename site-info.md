0ff5f2af6d40dbd85a79851f9f29d58599bd0cf5
earlman <earl.mangulabnan@gmail.com>
2023-06-27 08:23:13 -0400

    add new theme

---
+++ site-info.md
@@ -0,0 +1,2 @@
+resources: 
+https://quinndombrowski.com/blog/2022/05/07/hosting-eleventy-on-github-pages/
---
+++ site/_data/site.js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: "ECBS",
+  tagline: "Eleventy Classic Blog Starter",
+  url: process.env.URL || 'https://ecbs.com',
+  time: Date.now(),
+  locale: "en-GB",
+};
