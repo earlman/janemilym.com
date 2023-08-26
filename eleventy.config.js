8ae75a70fbd9dee5af821c6137ccbf44e1b8c427
earlman <earl.mangulabnan@gmail.com>
2023-08-26 09:20:21 -0500

    add sass support

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,10 @@
+const { EleventyRenderPlugin } = require("@11ty/eleventy");
+const sass = require("sass");
+const path = require("node:path");
+const browserslist = require("browserslist");
+const { transform, browserslistToTargets } = require("lightningcss");
 const yaml = require("js-yaml");
 require("dotenv").config();
-
-const { EleventyRenderPlugin } = require("@11ty/eleventy");
 
 const glob = require("fast-glob"),
    hljs = require("highlight.js"),
@@ -132,6 +135,43 @@ module.exports = (eleventyConfig) => {
          }
       );
    }
+
+   // Recognize Sass as a "template languages"
+   eleventyConfig.addTemplateFormats("sass");
+   // Compile Sass
+   eleventyConfig.addExtension("sass", {
+      outputFileExtension: "css",
+      compile: async function (inputContent, inputPath) {
+         // Skip files like _fileName.scss
+         let parsed = path.parse(inputPath);
+         if (parsed.name.startsWith("_")) {
+            return;
+         }
+
+         // Run file content through Sass
+         let result = sass.compileString(inputContent, {
+            loadPaths: [parsed.dir || "."],
+            sourceMap: true, // or true, your choice!,
+            syntax: "indented", // ! .SASS files don't work without this line
+         });
+
+         // Allow included files from @use or @import to
+         // trigger rebuilds when using --incremental
+         this.addDependencies(inputPath, result.loadedUrls);
+
+         let targets = browserslistToTargets(browserslist("defaults"));
+
+         return async () => {
+            let { code } = await transform({
+               code: Buffer.from(result.css),
+               minify: true,
+               sourceMap: true,
+               targets,
+            });
+            return code;
+         };
+      },
+   });
 
    // Passthrough
    eleventyConfig.addPassthroughCopy({
